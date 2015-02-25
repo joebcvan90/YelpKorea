8aa7a631bc0d1562805f7673518dd5542e312311
Han Nim Jang <josephnimz@gmail.com>
2015-02-25 17:58:34 +0900

    yml edited

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,10 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
+    :region					=> ENV["AWS_REGION"],
     :aws_access_key_id      => ENV["aws_access_key_id"],     # required
     :aws_secret_access_key  => ENV["aws_secret_access_key"], # required
   }
   config.fog_directory  = ENV["fog_directory"]               # required
-  storage = Fog::Storage.new(credentials.merge({:region => DEFAULT_REGION}))
-
 end
 
