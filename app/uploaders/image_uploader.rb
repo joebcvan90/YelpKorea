74ddfe8563c2ff5b32f42201328d7b8ca0e4307c
Han Nim Jang <josephnimz@gmail.com>
2015-02-25 18:12:36 +0900

    amazon settings

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,8 +8,12 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
 
-
-  storage :file
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :region					=> ENV["AWS_REGION"],
+    :region					=> ENV["aws_region"],
     :aws_access_key_id      => ENV["aws_access_key_id"],     # required
     :aws_secret_access_key  => ENV["aws_secret_access_key"], # required
   }
