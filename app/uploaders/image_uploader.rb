d4eea80b570cec801812f881b982eef895ee522c
Han Nim Jang <josephnimz@gmail.com>
2015-02-25 18:04:59 +0900

    back

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,13 +7,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
 
-  # storage :file
+
+  storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
